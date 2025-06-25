0f73e6847ec2caed3b3c2866f42cde2462239528
JinYunHwa <dbsghk320@gmail.com>
2025-06-25 22:52:15 +0900

    feat: add unoCSS

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Unocss from '@unocss/vite'
+import presetUno from '@unocss/preset-uno'
+import presetAttributify from '@unocss/preset-attributify'
+import presetIcons from '@unocss/preset-icons'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio',
-  plugins: [vue()],
+  base: '/portfolio/',
+  plugins: [
+    vue(),
+    Unocss({
+      presets: [
+        presetUno(),
+        presetAttributify(),
+        presetIcons({
+          scale: 1.2,
+          extraProperties: { 'display': 'inline-block' },
+        }),
+      ],
+    })
+  ]
 })
